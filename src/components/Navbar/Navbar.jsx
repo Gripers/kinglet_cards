eee8c60936feb1fade9a946d978b5175a37d4993
Gripers <gripers91@gmail.com>
2023-01-14 13:33:21 +0500

    dge

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,38 @@
+import styles from './Navbar.module.scss';
+
+import logo from '../../assets/images/logo.png';
+import { NavLink } from 'react-router-dom';
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>Hello Navbar!</h1>
+    <div className={styles.navbar__wrapper}>
+      <div className={styles.navbar__logo}>
+        <img src={logo} alt='' />
+      </div>
+      <div className={styles.navbar__links}>
+        <nav>
+          {[
+            {
+              title: 'Home',
+              route: '/',
+            },
+            {
+              title: 'Popular Designs',
+              route: '/popular-designs',
+            },
+            {
+              title: 'Contact Us',
+              route: '/contact-us',
+            },
+            {
+              title: 'About Us',
+              route: '/about-us',
+            },
+          ].map((nav) => (
+            <NavLink to={nav.route}>{nav.title}</NavLink>
+          ))}
+        </nav>
+      </div>
     </div>
   );
 };
